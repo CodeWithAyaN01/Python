5daf252d338f939a22473e48f29b9cab45ddd251
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-12-27 20:11:51 +0530

    Only one

--- DVP 2/10a.py
+++ DVP 2/10a.py
@@ -7,7 +7,3 @@ tsla = pd.read_csv(r'D:\\Download\\tesla-stock-price.csv')
 tsla.head()
 fig = px.line(tsla, x='date', y="close")
 fig.show()
-
-
-fig = px.line(tsla, x='date', y="close")
-fig.show()
--- test.py
+++
@@ -1,4 +0,0 @@
-import plotly.express as px
-df = px.data.iris()
-fig = px.scatter_3d(df,x = "sepal_width" , y = "sepal_length" , z = "petal_width",size = "petal_length",color = "species" , size_max =20 , opacity =1 )
-fig.show()
